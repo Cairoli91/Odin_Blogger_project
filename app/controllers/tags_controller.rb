82435531ce2d0f92f84ed758acf886847e1bd305
James <cairoli91@gmail.com>
2017-06-15 11:02:58 -0400

    Tags added. Having weird ID tag delete issue

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,29 @@
+class TagsController < ApplicationController
+
+  	def index
+  		@tags = Tag.all
+  	end
+
+  	def show
+  		@tag = Tag.find(params[:id])
+  	end
+
+  	def edit
+  		@tag = Tag.find(params[:id])
+  	end
+
+    def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+		flash.notice = "Tag '#{@tag.hash_tag}' Deleted"
+		redirect_to tags_path
+	end
+
+  	def update
+  		@tag = Tag.find(params[:id])
+  		@tag.update(tag_params)
+  		flash.notice = "Tag '#{@tag.hash_tag} Updated!'"
+  		redirect_to(tag_path)
+  	end
+
+end
